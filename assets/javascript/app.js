cfee08ee3a8175a114a83a283af829ddb36d67a5
Michelle <michelle@124-149-53-203.dyn.iinet.net.au>
2019-08-31 02:05:05 +1000

    Game working and basic HTML Visuals need to improve visuals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,152 +1,183 @@
 <!-- JAVASCRIPT  -->
-$(document).ready(function() {
 
-	// ******* NUMBER ARRAYS *******
-
-	// random computer variable array
-	var rand = [];
-
-	for (var r = 1; r < 120; r++) {
-		rand.push(r);
-	}
-
-	// animal numbers array
-	var animals = [];
-
-	for (var c = 1; c < 12; c++) {
-
-		animals.push(c);
-	}
-
-	// console.log(animals);
-
-	// ******* GLOBAL VARIABLES *******
-
-	// random variables selected by computer
-	var randNumber; // number to match
-	var animalNumbers = []; // for array of random animal values
-
-	var c1;
-	var c2;
-	var c3;
-	var c4;
-
-  var totalScore = 0; // user's score
-
-	var wins = 0;
-	var losses = 0;
-
-	// ******* FUNCTIONS *******
-
-	// pick a random number
-	function pickRandomNumber(arr) {
-
-		var x = arr[Math.floor(Math.random() * arr.length)];
-		randNumber = x;
-		$("#randomNumber").html(randNumber);
-
-		console.log("random number: " + randNumber);
-
-	} // END of pickRandomNumber function
-
-	// pick random numbers for animals
-
-	function pickRandomanimals(arr) {
-
-		for (var y = 0; y < 4; y++){
-
-			var a = arr[Math.floor(Math.random() * arr.length)];
-
-			animalNumbers.push(a);
+	$(document).ready(function(){
+  
+		// event listeners
+		$("#remaining-time").hide();
+		$("#start").on('click', trivia.startGame);
+		$(document).on('click' , '.option', trivia.guessChecker);
+		
+	  })
+	  
+	  var trivia = {
+		// trivia properties
+		correct: 0,
+		incorrect: 0,
+		unanswered: 0,
+		currentSet: 0,
+		timer: 20,
+		timerOn: false,
+		timerId : '',
+		// questions options and answers data
+		questions: {
+		  q1: 'What is captain Americas real name?',
+		  q2: 'In what year did the Avengers make their comic book debut?',
+		  q3: 'True or False: Thors weapon is a hammer?',
+		  q4: 'What kind of radiation caused Bruce Banner to transform into the Hulk when angry?',
+		  q5: "What color(s) is the first iron man armor?",
+		
+		},
+		options: {
+		  q1: ['Buck Rogers', 'Ted Rogers', 'Tony Stark', 'Steve Rogers'],
+		  q2: ['1987', '1963', '1975', '1959'],
+		  q3: ['True', 'False',],
+		  q4: ['x-ray', 'nuclear', 'ultraviolet', 'Gamma'],
+		  q5: ['Red & Gold','Grey & Silver','Red & Blue','Black'],
+		  
+		},
+		answers: {
+		  q1: 'Steve Rogers',
+		  q2: '1963',
+		  q3: 'True',
+		  q4: 'Gamma',
+		  q5: 'Grey & Silver',
+		  
+		},
+		// trivia methods
+		// method to initialize game
+		startGame: function(){
+		  // restarting game results
+		  trivia.currentSet = 0;
+		  trivia.correct = 0;
+		  trivia.incorrect = 0;
+		  trivia.unanswered = 0;
+		  clearInterval(trivia.timerId);
+		  
+		  // show game section
+		  $('#game').show();
+		  
+		  //  empty last results
+		  $('#results').html('');
+		  
+		  // show timer
+		  $('#timer').text(trivia.timer);
+		  
+		  // remove start button
+		  $('#start').hide();
+	  
+		  $('#remaining-time').show();
+		  
+		  // ask first question
+		  trivia.nextQuestion();
+		  
+		},
+		// method to loop through and display questions and options 
+		nextQuestion : function(){
+		  
+		  // set timer to 20 seconds each question
+		  trivia.timer = 20;
+		   $('#timer').removeClass('last-seconds');
+		  $('#timer').text(trivia.timer);
+		  
+		  // to prevent timer speed up
+		  if(!trivia.timerOn){
+			trivia.timerId = setInterval(trivia.timerRunning, 1000);
+		  }
+		  
+		  // gets all the questions then indexes the current questions
+		  var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+		  $('#question').text(questionContent);
+		  
+		  // an array of all the user options for the current question
+		  var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+		  
+		  // creates all the trivia guess options in the html
+		  $.each(questionOptions, function(index, key){
+			$('#options').append($('<button class="option btn btn-warning btn-lg">'+key+'</button>'));
+		  })
+		  
+		},
+		// method to decrement counter and count unanswered if timer runs out
+		timerRunning : function(){
+		  // if timer still has time left and there are still questions left to ask
+		  if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+			$('#timer').text(trivia.timer);
+			trivia.timer--;
+			  if(trivia.timer === 4){
+				$('#timer').addClass('last-seconds');
+			  }
+		  }
+		  // the time has run out and increment unanswered, run result
+		  else if(trivia.timer === -1){
+			trivia.unanswered++;
+			trivia.result = false;
+			clearInterval(trivia.timerId);
+			resultId = setTimeout(trivia.guessResult, 1000);
+			$('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+		  }
+		  // if all the questions have been shown end the game, show results
+		  else if(trivia.currentSet === Object.keys(trivia.questions).length){
+			
+			// adds results of game (correct, incorrect, unanswered) to the page
+			$('#results')
+			  .html('<h3>Thank you for playing!</h3>'+
+			  '<p>Correct: '+ trivia.correct +'</p>'+
+			  '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+			  '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+			  '<p>Please play again!</p>');
+			
+			// hide game sction
+			$('#game').hide();
+			
+			// show start button to begin a new game
+			$('#start').show();
+		  }
+		  
+		},
+		// method to evaluate the option clicked
+		guessChecker : function() {
+		  
+		  // timer ID for gameResult setTimeout
+		  var resultId;
+		  
+		  // the answer to the current question being asked
+		  var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+		  
+		  // if the text of the option picked matches the answer of the current question, increment correct
+		  if($(this).text() === currentAnswer){
+			// turn button green for correct
+			$(this).addClass('btn-success').removeClass('btn-warning');
+			
+			trivia.correct++;
+			clearInterval(trivia.timerId);
+			resultId = setTimeout(trivia.guessResult, 1000);
+			$('#results').html('<h3>Correct Answer!</h3>');
+		  }
+		  // else the user picked the wrong option, increment incorrect
+		  else{
+			// turn button clicked red for incorrect
+			$(this).addClass('btn-danger').removeClass('btn-warning');
+			
+			trivia.incorrect++;
+			clearInterval(trivia.timerId);
+			resultId = setTimeout(trivia.guessResult, 1000);
+			$('#results').html('<h3>Correct Answer '+ currentAnswer +'</h3>');
+		  }
+		  
+		},
+		// method to remove previous question results and options
+		guessResult : function(){
+		  
+		  // increment to next question set
+		  trivia.currentSet++;
+		  
+		  // remove the options and results
+		  $('.option').remove();
+		  $('#results h3').remove();
+		  
+		  // begin next question
+		  trivia.nextQuestion();
+		   
 		}
-    // check which numbers have been picked
-		console.log("animal numbers: " + animalNumbers);
-
-	} // END of pickRandomanimals function
-
-	function animalValues(arr) {
-
-		// change value of each animal button according to array
-		for (i = 0; i < arr.length; i++) {
-
-		$("#button-" + (i+1)).attr("value", arr[i]);
-		console.log(this);
-		}
-		c1 = arr[0];
-		c2 = arr[1];
-		c3 = arr[2];
-		c4 = arr[3];
-	} // END of animalValues function
-
-	function gameReset(x) {
-
-		animalNumbers = []; // clears animal number values
-
-		pickRandomNumber(rand);
-
-		pickRandomanimals(animals);
-
-		animalValues(animalNumbers);
-
-		totalScore = 0;
-		$("#totalNumber").html(totalScore);
-
-		alert(x);
-	} // END of gameReset function
-
-	// *** GAME SETTINGS AT START ***
-
-	pickRandomNumber(rand); // random number to match
-	pickRandomanimals(animals); // array of random animal values
-	animalValues(animalNumbers);
-
-		// animal button functions
-
-		$("#button-1").on("click", function() {
-
-			totalScore += c1;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-2").on("click", function() {
-
-			totalScore += c2;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-3").on("click", function() {
-
-			totalScore += c3;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-4").on("click", function() {
-
-			totalScore += c4;
-			$("#totalNumber").html(totalScore);
-		});
-
-	$("button").on("click", function() {
-		// this is what happens if the user wins
-		if (totalScore == randNumber) {
-
-			wins++;
-			console.log(totalScore);
-			$("#totalNumber").html(totalScore);
-			$("#wins").html("Wins: " + wins);
-
-
-			setTimeout(function() {gameReset("YOU WIN!!")}, 200);
-		}
-
-		else if (totalScore > randNumber){
-
-			losses++;
-			$("#totalNumber").html(totalScore);
-			$("#losses").html("Losses: " + losses);
-
-			setTimeout(function() {gameReset("WOMP-WOMP...YOU LOSE!")}, 200);
-		}
-	});
-
-}); // end of script
+	  
+	  }
